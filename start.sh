8875670a75cbb3ab0db3748beb786f8529df00fd
Gleb Arkhipov <gleber1995@gmail.com>
2022-03-12 18:49:16 +0300

    show logs after restart and start

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ if [[ $1 != "-no-build" ]] ; then
 fi
 if [[ $? == 0 ]] ; then
   docker-compose -p "" up -d --force-recreate
+  docker logs -f backend
 fi
 
 function rebuild_restart_backend {
   ./gradlew clean build -x test && docker build -t moneysearch-backend:latest .
   docker-compose rm -sf backend
   docker-compose up -d
+  docker logs -f backend
 }
